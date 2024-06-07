f9eec94bed64ec141719e73d0fc1b1af367b8921
Leggo15 <WildQard.no@gmail.com>
2024-06-07 23:15:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/LunarMap/',
-  plugins: [react()]
-})
+  base: "/LunarMap/",
+  plugins: [react()],
+});
