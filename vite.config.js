cc84d944a29ec1096465e5f4b2e6dbe1f696d87f
Leggo15 <WildQard.no@gmail.com>
2024-06-07 23:15:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 // vite.config.js
-export default {
-  base: "/LunarSurfaceMap/",
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/LunarMap/',
+  plugins: [react()]
+})
