040eba87eecbadf8decff1aea5d3276042f575b1
Leggo15 <WildQard.no@gmail.com>
2024-08-12 16:59:25 +0200

    ran prettier

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/LunarMap/', // This is crucial for GitHub Pages
+  base: "/LunarMap/", // This is crucial for GitHub Pages
   plugins: [react()], // If you're using React, make sure this plugin is included
   build: {
-    outDir: 'dist', // The default output directory
+    outDir: "dist", // The default output directory
     rollupOptions: {
       output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: "assets/[name].[hash][extname]",
+        chunkFileNames: "assets/[name].[hash].js",
+        entryFileNames: "assets/[name].[hash].js",
       },
     },
   },
